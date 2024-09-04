80db8bbdbddc0dfd0f90afb43c42032a8645b98e
Phoolis <carlsonps@gmail.com>
2024-09-04 20:12:43 +0300

    Add functionality to Add and Delete books

--- src/main/java/paul24/bookstore/web/BookController.java
+++ src/main/java/paul24/bookstore/web/BookController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import paul24.bookstore.model.Book;
 import paul24.bookstore.model.BookRepository;
+
 
 
 @Controller
@@ -25,6 +29,26 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
     }
+
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/saveBook")
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+    
+    @GetMapping("delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
+    }
+    
+    
     
     
 }
