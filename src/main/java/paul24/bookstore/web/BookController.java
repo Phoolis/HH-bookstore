32e1fc7b53633024d1f9c146433e896923224d61
Phoolis <carlsonps@gmail.com>
2024-09-04 19:30:17 +0300

    Add booklist endpoint and GetMapping to show all books

--- src/main/java/paul24/bookstore/web/BookController.java
+++ src/main/java/paul24/bookstore/web/BookController.java
@@ -1,6 +1,7 @@
 package paul24.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,5 +21,12 @@ public class BookController {
     public String getMethodName(@RequestParam String param) {
         return new String();
     }
+
+    @GetMapping("/booklist")
+    public String showBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
+    }
+    
     
 }
