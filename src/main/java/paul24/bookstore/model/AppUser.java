d4561f29068215b582e4b2c181cf657a8d358f13
Phoolis <carlsonps@gmail.com>
2024-09-25 19:04:03 +0300

    Add AppUser entity class and repo

---
+++ src/main/java/paul24/bookstore/model/AppUser.java
@@ -0,0 +1,69 @@
+package paul24.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "UserTable")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    public AppUser() {
+    }
+
+    public AppUser(String passwordHash, String role, String username) {
+        this.passwordHash = passwordHash;
+        this.role = role;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
---
+++ src/main/java/paul24/bookstore/model/AppUserRepository.java
@@ -0,0 +1,8 @@
+package paul24.bookstore.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AppUserRepository extends CrudRepository<AppUser, Long> {
+
+    AppUser findByUsername(String username);
+}
